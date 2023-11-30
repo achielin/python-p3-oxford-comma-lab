582a39120ff2edc6eb3faa1c7ac7dd25db3eb438
linder Opondo <achielinder@gmail.com>
2023-11-30 21:40:39 +0300

    completed assignments

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,1 +1,11 @@
-
+def oxford_comma(items):
+    return None
+    length = len(items)
+    if length == 0:
+        return ""
+    elif length == 1:
+        return items[0]
+    elif length == 2:
+        return items[0] + " and " + items[1]
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
